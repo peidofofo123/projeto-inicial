dc77bce14a25b3e04f0221d8b9bad2cf0f61e9d7
peidofofo123 <mikaellysilvalima16@gmail.com>
2024-03-21 09:13:42 -0300

    buscacep

--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
-var botao = document.getElementById("botao")
-botao = addEventListener("click", alertar);
+var botao = document.getElementById("botao");
+botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+var saida = document.getElementById("saida-de-dados");
+ 
+function alertar(event){
+    //alert("Você clicou no botão!" + event);
 
-function alertar(){
-    alert("Você clicou no botão!!!");
+    const url = `https://viacep.com.nr/ws/${cep.value}/json;
+    fetch(url)
+    .then(resposta=>resposta.json())
+    .then(dados=>alert(dados.barro))
+
+    saida.innerText = "Nome: " + nome.value +
+    "\n Email: " + email.value +
+    "\n Telefone: " + telefone.value +
+    "\n CEP: " + cep.value +
+    "\n Logradouro: " + logradouro.value +
+    "\n Número: " + numero.value +
+    "\n Complemento: " + complemento.value +
+    "\n Bairro: " + bairro.value +
+    "\n Cidade: " + cidade.value +
+    "\n Estado: " + estado.value
+    ;
+    
 }
